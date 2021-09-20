3c6bb635124d8f45b011d8043757c301edcefdb0
onikan27 <ude0721juji@icloud.com>
2021-09-20 16:22:58 +0900

    feat: Add user controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express from "express";
+import userController from "./controllers/userController";
 
-const app: express.Express = express();
+const app = express();
+app.use(express.json());
+
+app.use("/users", userController);
 
 app.get("/", (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Hello Express\n")
-})
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello express\n");
+});
 
-export default app
+export default app;
